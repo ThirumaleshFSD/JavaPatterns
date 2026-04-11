b89363e0bb88a59c40bf39a876ef0f1fbc0f84ca
ThirumaleshFSD <sirisampadha939184@gmail.com>
2026-04-12 00:16:14 +0530

    the substring problem is added

---
+++ SubStrings.java
@@ -0,0 +1,14 @@
+public class SubStrings {
+    public static void main(String[] args) {
+        String s="abc";
+        for(int i=0;i<s.length();i++){
+            for(int j=i;j<=s.length();j++){
+
+                for(int k=i;k<j;k++){
+                    System.out.print(s.charAt(k));
+                }
+                System.out.println();
+            }
+        }
+    }
+}
